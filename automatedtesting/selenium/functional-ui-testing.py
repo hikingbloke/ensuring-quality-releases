4212b2011ec8de0b96af14a0c847d06b49945a95
Bipin Sawant <bipin.sawant@gmail.com>
2021-02-19 20:43:02 -0800

    changes to python script

--- automatedtesting/selenium/functional-ui-testing.py
+++ automatedtesting/selenium/functional-ui-testing.py
@@ -3,24 +3,24 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support.expected_conditions import presence_of_element_located
-from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.options import Options as ChromeOptions
 from datetime import datetime
 
-chrome_options = Options()
-chrome_options.add_argument("--headless")
-
-with webdriver.Chrome(options=chrome_options) as driver:
-    wait = WebDriverWait(driver, 10)
+def login (user, password):
+    print ('Starting the browser...')
+    chrome_options = ChromeOptions()
+    chrome_options.add_argument("--headless") 
+    driver = webdriver.Chrome(options=chrome_options)
 
     driver.get('https://www.saucedemo.com/')
     username_input = driver.find_element(By.ID, "user-name")
     password_input = driver.find_element(By.ID, "password")
-    username_input.send_keys("standard_user")
-    password_input.send_keys("secret_sauce" + Keys.RETURN)
+    username_input.send_keys(user)
+    password_input.send_keys(password + Keys.RETURN)
     
     print("Logging success")
-    print("user: standard_user")
-    print("password: secret_sauce")
+    print("user: " + user)
+    print("password: " + password)
 
     print("Getting Inventory list")
     inventory_items = driver.find_elements(By.CLASS_NAME, "inventory_item")
@@ -44,3 +44,5 @@ with webdriver.Chrome(options=chrome_options) as driver:
         button = item.find_element(By.CSS_SELECTOR, "button[class='btn_secondary cart_button']").click()
 
     print("Done!")
+
+login('standard_user', 'secret_sauce')
